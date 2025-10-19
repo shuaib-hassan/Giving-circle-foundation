4e750720b12551b9c6a019b85b7c39509ef094b7
shuaib-hassan <shuaib.hassan@student.moringaschool.com>
2025-10-19 21:29:39 +0300

    Initial commit: Build complete Giving Circle Foundation website

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+// JavaScript for smooth scrolling and other future functionalities
+
+const navSlide = () => {
+    const burger = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
+
+$(document).ready(function(){
+    $('.stories-container').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
